508e332ea16d7ccd96d0a76fd2844c75f6a301fa
antonko-de <anton_kostov@protonmail.com>
2023-03-04 17:34:30 +0200

    lets do this course mean machine

---
+++ first_steps_oop/car.py
@@ -0,0 +1,17 @@
+'''
+Create a class called Car. Upon initialization, it should receive a name, model, and engine (all strings). 
+Create a method called get_info() which will return a string in the following format:
+"This is {name} {model} with engine {engine}".
+'''
+
+class Car:
+    def __init__(self, name:str, model:str, engine:str) -> None:
+        self.name = name
+        self.model = model
+        self.engine = engine
+        
+        
+    def get_info(self):
+        return (f"This is {self.name} {self.model} with engine {self.engine}")
+    
+
